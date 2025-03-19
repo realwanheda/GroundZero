5e3607624680a2020868806f7666438a0816e58b
Vabhav Tripathi <95403863+realwanheda@users.noreply.github.com>
2025-03-20 03:29:15 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://groundzero.onrender.com",
   withCredentials: true,
   headers: {
     Accept: "application/json",
